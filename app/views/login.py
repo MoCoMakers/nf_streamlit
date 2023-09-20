ccc072ca210811b3a2f68752b298447d728ae1cf
Chuba Oraka <chubaoraka@users.noreply.github.com>
2023-09-20 18:24:02 -0400

    Fix Duplicate Widget errors that results from reusing/extending a widget
    Notes:
    I rearranged the import functions to address the Duplicate Widget errors that results from
    reusing a streamlit class.
    In the reference section, I referenced a number of sources that helped me get to the error stacktraces
    that are usually hidden because of error handling
    
    Fixed Error Messages:
    To fix this, please make sure that the `key` argument is unique for each
    widget you create.
    2024-06-19 21:24:08.175 Uncaught app exception
    Traceback (most recent call last):
      File "/usr/local/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 600, in _run_script
        exec(code, module.__dict__)
      File "/app/Home.py", line 13, in <module>
        login.loginapp()
      File "/app/views/login.py", line 25, in loginapp
        newauthenticator = SignUp(
      File "/app/views/signupform.py", line 20, in __init__
        super().__init__(*attributes)
      File "/usr/local/lib/python3.9/site-packages/streamlit_authenticator/authenticate/__init__.py", line 53, in __init__
        self.cookie_handler             =   CookieHandler(cookie_name,
      File "/usr/local/lib/python3.9/site-packages/streamlit_authenticator/authenticate/cookie/__init__.py", line 39, in __init__
        self.cookie_manager         =   stx.CookieManager()
      File "/usr/local/lib/python3.9/site-packages/extra_streamlit_components/CookieManager/__init__.py", line 22, in __init__
        self.cookies = self.cookie_manager(method="getAll", key=key, default={})
      File "/usr/local/lib/python3.9/site-packages/streamlit/components/v1/custom_component.py", line 55, in __call__
        return self.create_instance(*args, default=default, key=key, **kwargs)
      File "/usr/local/lib/python3.9/site-packages/streamlit/runtime/metrics_util.py", line 397, in wrapped_func
        result = non_optional_func(*args, **kwargs)
      File "/usr/local/lib/python3.9/site-packages/streamlit/components/v1/custom_component.py", line 215, in create_instance
        return_value = marshall_component(dg, element)
      File "/usr/local/lib/python3.9/site-packages/streamlit/components/v1/custom_component.py", line 190, in marshall_component
        component_state = register_widget(
      File "/usr/local/lib/python3.9/site-packages/streamlit/runtime/state/widgets.py", line 167, in register_widget
        return register_widget_from_metadata(metadata, ctx, widget_func_name, element_type)
      File "/usr/local/lib/python3.9/site-packages/streamlit/runtime/state/widgets.py", line 200, in register_widget_from_metadata
        raise DuplicateWidgetID(
    streamlit.errors.DuplicateWidgetID: There are multiple widgets with the same `key='init'`.
    
    References:
    How to print exception stack trace in Python? - GeeksforGeeks
    https://www.geeksforgeeks.org/how-to-print-exception-stack-trace-in-python/
    
    python - Is there a built-in function to print all the current properties and values of an object? - Stack Overflow
    https://stackoverflow.com/questions/192109/is-there-a-built-in-function-to-print-all-the-current-properties-and-values-of-a

--- app/views/login.py
+++ app/views/login.py
@@ -12,27 +12,23 @@ credentials_in_database = get_credentials()
 
 def loginapp():
     global credentials_in_database
-    authenticator = stauth.Authenticate(
-        copy.deepcopy(credentials_in_database),
-        st.secrets["cookie"]["name"],
-        st.secrets["cookie"]["key"],
-        st.secrets["cookie"]["expiry_days"],
-        None,
-    )
-    newauthenticator = SignUp(
-        copy.deepcopy(credentials_in_database),
-        st.secrets["cookie"]["name"],
-        st.secrets["cookie"]["key"],
-        st.secrets["cookie"]["expiry_days"],
-        None,
-    )
+    
+    
     if "signup" not in st.session_state:
         st.session_state.signup = False
 
     def toggle_signup_login():
         st.session_state.signup = False if st.session_state.signup else True
 
-    if st.session_state.signup:
+    if  st.session_state.signup:
+        newauthenticator = SignUp(
+            copy.deepcopy(credentials_in_database),
+            st.secrets["cookie"]["name"],
+            st.secrets["cookie"]["key"],
+            st.secrets["cookie"]["expiry_days"],
+            None,
+        )
+
         try:
             hashed_password, email_of_registered_user, username_of_registered_user, name_of_registered_user = newauthenticator.register_user(
                 "main", pre_authorization=False, fields={"Form name": "Sign Up"}
@@ -45,7 +41,15 @@ def loginapp():
                 st.button(":blue[Log In instead]", on_click=toggle_signup_login)
         except Exception as e:
             st.error(e)
+        
     else:
+        authenticator = stauth.Authenticate(
+            copy.deepcopy(credentials_in_database),
+            st.secrets["cookie"]["name"],
+            st.secrets["cookie"]["key"],
+            st.secrets["cookie"]["expiry_days"],
+            None,
+        )
         name, authentication_status, username = authenticator.login(
             "main", fields={"Form name": "Login"}
         )
