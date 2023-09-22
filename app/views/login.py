9ede38d6dabc2b2e000d3b36d6f2dfa84311d68c
Chuba Oraka <chubaoraka@users.noreply.github.com>
2023-09-22 19:42:31 -0400

    Add sign up form using Streamlit-Authenticator package
    References:
    Streamlit-Authenticator, Part 2: Adding advanced features to your authentication component
    https://blog.streamlit.io/streamlit-authenticator-part-2-adding-advanced-features-to-your-authentication-component/
    
    Streamlit-Authenticator, Part 1: Adding an authentication component to your app
    https://blog.streamlit.io/streamlit-authenticator-part-1-adding-an-authentication-component-to-your-app/
    
    How to Add A Login & Sign-up Section to Streamlit Blog App – JCharisTech
    https://blog.jcharistech.com/2020/05/30/how-to-add-a-login-section-to-streamlit-blog-app/
    
    Streamlit-Authenticator/streamlit_authenticator/authenticate.py at main · mkhorasani/Streamlit-Authenticator
    https://github.com/mkhorasani/Streamlit-Authenticator/blob/main/streamlit_authenticator/authenticate.py
    https://github.com/mkhorasani/Streamlit-Authenticator/blob/main/streamlit_authenticator/authenticate.py#L317
    
    st.button - Streamlit Docs
    https://docs.streamlit.io/library/api-reference/widgets/st.button
    
    Button behavior and examples - Streamlit Docs
    https://docs.streamlit.io/library/advanced-features/button-behavior-and-examples
    
    Add statefulness to apps - Streamlit Docs
    https://docs.streamlit.io/library/advanced-features/session-state#initialization

--- app/views/login.py
+++ app/views/login.py
@@ -14,13 +14,30 @@ def loginapp():
         config['cookie']['expiry_days'],
         config['preauthorized']
     )
-    name, authentication_status, username = authenticator.login('Login', 'main')
+    if 'signup' not in st.session_state:
+        st.session_state.signup = False
 
-    if authentication_status:
-        authenticator.logout('Logout', 'main')
-        st.write(f'Welcome *{name}*')
-        eda()
-    elif authentication_status == False:
-        st.error('Username/password is incorrect')
-    elif authentication_status == None:
-        st.warning('Please enter your username and password')
+    def click_button():
+        st.session_state.signup = False if st.session_state.signup else True
+
+    if st.session_state.signup:
+        try:
+            if authenticator.register_user('Sign Up', 'main'):
+                st.success('User registered successfully')
+            st.button('Log In instead', on_click=click_button)
+        except Exception as e:
+            st.error(e)
+    else:
+        name, authentication_status, username = authenticator.login('Login', 'main')
+        if not authentication_status:
+            st.button('Sign Up instead', on_click=click_button)
+
+    if not st.session_state.signup and "authentication_status" in st.session_state:
+        if st.session_state["authentication_status"]:
+            authenticator.logout('Logout', 'main')
+            st.write(f'Welcome *{name}*')
+            eda()
+        elif st.session_state["authentication_status"] == False:
+            st.error('Username/password is incorrect')
+        elif st.session_state["authentication_status"] == None:
+            st.warning('Please enter your username and password')
