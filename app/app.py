eb1057abe53927b042b51a5dc40230d16223ed9e
Chuba Oraka <chubaoraka@users.noreply.github.com>
2023-10-13 08:34:30 -0400

    Set page title
    Notes:
    The Streamlit framework does not provide a reliable way of dynamically changing the page title once set with `st.set_page_config`
    
    References:
    How to remove "· Streamlit" from the app title? - Streamlit Docs
    https://docs.streamlit.io/knowledge-base/using-streamlit/remove-streamlit-app-title
    
    Change page title & favicon after first st call · Issue #2216 · streamlit/streamlit
    https://github.com/streamlit/streamlit/issues/2216
    https://github.com/streamlit/streamlit/issues/2216#issuecomment-1526942763

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from views import login
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title="Hack4NF Drug Response Data Visualization")
 login.loginapp()
