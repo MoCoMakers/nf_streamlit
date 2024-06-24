c486b6ae1637a32703c946e97769053c1329e3bf
Chuba Oraka <chubaoraka@users.noreply.github.com>
2024-06-24 05:47:49 -0400

    Using non-native pages package to have more fine-grained control of Pages
    Notable Terminal Command:
    `pip install st-pages`
    
    Notes:
    The native `StreamlitPage` does not play well with`streamlit-authenticator`.
    It makes data available to users who are not logged in. I used `st-pages` to
    gain more control of the pages and make them available only when logged in.
    It however leads to a breaking change where one logged-in user make the site
    available to subsequent users without logging in
    
    Breaking changes:
    - One logged-in user make the site available to subsequent users without logging in
    
    References:
    How to use mutipages and authencation together - 🎈 Using Streamlit - Streamlit
    https://discuss.streamlit.io/t/how-to-use-mutipages-and-authencation-together/44957
    
    How to hide all pages before login - 🎈 Using Streamlit - Streamlit
    https://discuss.streamlit.io/t/how-to-hide-all-pages-before-login/32508/26
    
    GitHub - blackary/st_pages: An experimental version of Streamlit Multi-Page Apps
    https://github.com/blackary/st_pages?tab=readme-ov-file
    https://github.com/blackary/st_pages?tab=readme-ov-file#installation
    
    Creating multipage apps using the `pages/` directory - Streamlit Docs
    https://docs.streamlit.io/develop/concepts/multipage-apps/pages-directory
    
    Define multipage apps with st.Page and st.navigation - Streamlit Docs
    https://docs.streamlit.io/develop/concepts/multipage-apps/page-and-navigation
    
    GitHub - blackary/st_pages: An experimental version of Streamlit Multi-Page Apps
    https://github.com/blackary/st_pages?tab=readme-ov-file
    
    Streamlit
    https://st-pages.streamlit.app/
    
    Issues · blackary/st_pages · GitHub
    https://github.com/blackary/st_pages/issues

--- app/views/signed_in_landing.py
+++ app/views/signed_in_landing.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 def landing_page():
     st.write("Choose one of the pages in the sidebar to get started.")
+    show_pages(
+        [
+            Page("visualizations/Delta_S_Prime.py", "Delta S Prime", "📊", use_relative_hash=True),
+            Page("visualizations/MIPE_3_0.py", "MIPE 3.0", "📈", use_relative_hash=True),
+        ]
+    )
